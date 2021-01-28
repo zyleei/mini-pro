cd603e9b295688513cd0d5dfe7a84757c3b3a562
Zhiyuan Lei <zlei@baobab.local>
2021-01-28 16:11:55 -0600

    add tpumac

---
+++ mac/tpumac.sv
@@ -0,0 +1,16 @@
+module tpumac
+ #(parameter BITS_AB=8,
+   parameter BITS_C=16)
+  (
+   input clk, rst_n, WrEn, en,
+   input signed [BITS_AB-1:0] Ain,
+   input signed [BITS_AB-1:0] Bin,
+   input signed [BITS_C-1:0] Cin,
+   output reg signed [BITS_AB-1:0] Aout,
+   output reg signed [BITS_AB-1:0] Bout,
+   output reg signed [BITS_C-1:0] Cout
+  );
+
+
+
+
